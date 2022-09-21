73c071b9dd888321846f9ecc48ffec6097be8be6
Anthony Chen <orbit@ad.unc.edu>
2022-09-21 13:54:26 -0400

    only_evens

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -5,4 +5,12 @@ __author__ = "730548982"
 
 def only_evens(input_list: list[int]) -> list[int]:
     """This function returns the even numbers in a list."""
-    
+    output_list: list[int] = []
+    i: int = 0
+    while i < len(input_list):
+        if input_list[i] % 2 == 0:
+            output_list.append(input_list[i])
+            i += 1
+        else:
+            i += 1
+    return output_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -12,13 +12,13 @@ def test_only_evens_empty() -> None:
 
 
 def test_only_evens_odds() -> None:
-    """This function checks if only_evens works with an empty list."""
-    test_list: list[int] = [1,2,3]
+    """This function checks if only_evens works with odd numbers."""
+    test_list: list[int] = [1,5,3]
     assert only_evens(test_list) == []
 
 
 def test_only_evens_evens() -> None:
-    """This function checks if only_evens works with an empty list."""
+    """This function checks if only_evens works with even numbers."""
     test_list: list[int] = [4,4,4]
-    assert only_evens(test_list) == []
+    assert only_evens(test_list) == [4,4,4]
 
