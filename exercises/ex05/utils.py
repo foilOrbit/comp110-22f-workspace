7b90e241014579f618363f9186d27537a6e786e1
Anthony Chen <orbit@ad.unc.edu>
2022-09-21 14:22:06 -0400

    concat and sub

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -14,3 +14,25 @@ def only_evens(input_list: list[int]) -> list[int]:
         else:
             i += 1
     return output_list
+
+
+def concat(list_one: list[int], list_two: list[int]) -> list[int]:
+    """This function concatenates list_two to the end of list_one."""
+    output_list: list[int] = list_one
+    i: int = 0
+    while i < len(list_two):
+        output_list.append(list_two[i])
+        i += 1
+    return output_list
+
+
+def sub(input_list: list[int], start: int, end: int) -> list[int]:
+    """This function creates a subset list beginning at start and finishing at end - 1."""
+    output_list: list[int] = []
+    i: int = start
+    if start < 0:
+        i = 0
+    while i < end and i < len(input_list):
+        output_list.append(input_list[i])
+        i += 1
+    return output_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -2,7 +2,7 @@
 
 __author__ = "730548982"
 
-from utils import only_evens
+from utils import only_evens, concat, sub
 
 
 def test_only_evens_empty() -> None:
@@ -22,3 +22,41 @@ def test_only_evens_evens() -> None:
     test_list: list[int] = [4,4,4]
     assert only_evens(test_list) == [4,4,4]
 
+
+def test_concat_empty() -> None:
+    """This function checks if concat works with two empty lists."""
+    test_list_one: list[int] = []
+    test_list_two: list[int] = []
+    assert concat(test_list_one, test_list_two) == []
+
+
+def test_only_evens_same_length() -> None:
+    """This function checks if concat works with lists of the same length."""
+    test_list_one: list[int] = [1,2,3]
+    test_list_two: list[int] = [4,5,6]
+    assert concat(test_list_one, test_list_two) == [1,2,3,4,5,6]
+
+
+def test_concat_diff_length() -> None:
+    """This function checks if concat works with lists of different lengths."""
+    test_list_one: list[int] = [1,2,3,4,5]
+    test_list_two: list[int] = [6]
+    assert concat(test_list_one, test_list_two) == [1,2,3,4,5,6]
+
+
+def test_sub_empty() -> None:
+    """This function checks if sub works with an empty list."""
+    test_list: list[int] = []
+    assert sub(test_list,1,3) == []
+
+
+def test_sub_output_one() -> None:
+    """This function checks if sub works with two indexes one unit apart."""
+    test_list: list[int] = [1,2,3,4,5,6,7,8]
+    assert sub(test_list,1,2) == [2]
+
+
+def test_sub_output_multi() -> None:
+    """This function checks if sub works with two indexes multiple units apart."""
+    test_list: list[int] = [1,2,3,4,5,6,7,8]
+    assert sub(test_list,0,5) == [1,2,3,4,5]
